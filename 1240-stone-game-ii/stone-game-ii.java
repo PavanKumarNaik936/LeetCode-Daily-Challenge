3f9ba13529e3918b40e6d5099580e6881c61db7f
PavanKumarNaik936 <pavankumar1908naik@gmail.com>
2026-08-10 15:52:35 +0530

    Time: 16 ms (24.40%) | Memory: 46.6 MB (26.45%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.java
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -1,42 +1,37 @@
 class Solution {
-
     public int stoneGameII(int[] piles) {
-        // Store the suffix sum of all array elements.
-        int[] suffixSum = Arrays.copyOf(piles, piles.length);
-
-        for (int i = suffixSum.length - 2; i >= 0; i--) {
-            suffixSum[i] += suffixSum[i + 1];
+        // 0->alice turn    1->bobs turn 
+        int[][][]dp = new int[piles.length][piles.length+1][2];
+        for(int[][]R:dp){
+            for(int[]r:R){
+                Arrays.fill(r,-1);
+            }
         }
-        return maxStones(suffixSum, 1, 0, new int[piles.length][piles.length]);
+        return solve(0,0,1,piles,dp);
     }
-
-    private int maxStones(
-        int[] suffixSum,
-        int maxTillNow,
-        int currIndex,
-        int[][] memo
-    ) {
-        // If currIndex + 2*maxTillNow lies outside the array, pick all remaining stones.
-        if (currIndex + 2 * maxTillNow >= suffixSum.length) {
-            return suffixSum[currIndex];
+    public int solve(int i,int person,int M,int[]piles,int[][][]dp){
+        if(i>=piles.length)
+            return 0;
+        if(dp[i][M][person]!=-1)
+            return dp[i][M][person];
+        int best;
+        if(person==0){
+            best = 0;
+            int sum = 0;
+            for(int x=1; x<=2*M && i+x<=piles.length ; x++){
+                int newM = Math.max(x,M);
+                sum+=piles[i+x-1];
+                int alice = sum+solve(i+x,1,newM,piles,dp);
+                best = Math.max(best,alice);
+            }
+        }else{
+            best = Integer.MAX_VALUE;
+            for(int x=1;x<=2*M && i+x<=piles.length ;x++){
+                int newM = Math.max(x,M);
+                int alice = solve(i+x,0,newM,piles,dp);
+                best = Math.min(best,alice);
+            }
         }
-        if (memo[currIndex][maxTillNow] > 0) return memo[currIndex][maxTillNow];
-        int res = Integer.MAX_VALUE;
-        // Find the minimum value res for the next move possible.
-        for (int i = 1; i <= 2 * maxTillNow; i++) {
-            res = Math.min(
-                res,
-                maxStones(
-                    suffixSum,
-                    Math.max(i, maxTillNow),
-                    currIndex + i,
-                    memo
-                )
-            );
-        }
-        // Memoize the difference of suffixSum[p] and res. This denotes the maximum
-        // stones that can be picked.
-        memo[currIndex][maxTillNow] = suffixSum[currIndex] - res;
-        return memo[currIndex][maxTillNow];
+        return dp[i][M][person]=best;
     }
 }
