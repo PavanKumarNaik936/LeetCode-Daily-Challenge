22a742416a54e65d8f69b05a26dd70ca2206ddcb
PavanKumarNaik936 <pavankumar1908naik@gmail.com>
2026-08-10 16:22:39 +0530

    Time: 104 ms (5.17%) | Memory: 110.9 MB (5.70%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,26 +1,45 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
-        int n = stoneValue.length;
-        int[] dp = new int[n + 1];
-
-        // dp[i] = maximum score difference current player can achieve
-        dp[n] = 0;
-
-        for (int i = n - 1; i >= 0; i--) {
-            dp[i] = Integer.MIN_VALUE;
-            int sum = 0;
-
-            for (int k = 0; k < 3 && i + k < n; k++) {
-                sum += stoneValue[i + k];
-                dp[i] = Math.max(dp[i], sum - dp[i + k + 1]);
+        long totalValue = 0;
+        long[][]dp = new long[stoneValue.length][2];
+        for(long[] r:dp){
+            Arrays.fill(r,-1);
+        }
+        for(int x:stoneValue){
+            totalValue+=x;
+        }
+        long alice = findAlice(0,0,stoneValue,dp);
+        long bob = totalValue-alice;
+        if(alice>bob){
+            return "Alice";
+        }else if(alice<bob){
+            return "Bob";
+        }else
+            return "Tie";
+    }
+    // this method will find max alice value
+    public long findAlice(int i,int person,int[]stoneValue,long[][]dp){
+        if(i>=stoneValue.length)
+            return 0;
+        if(dp[i][person]!=-1)
+            return dp[i][person];
+        long best;
+        if(person==0){
+            //alice turn 
+            best = Long.MIN_VALUE;
+            long sum = 0;
+            for(int x=1;x<=3 && x+i<=stoneValue.length;x++){
+                sum+=stoneValue[i+x-1];
+                long alice = sum+findAlice(i+x,1,stoneValue,dp);
+                best = Math.max(best,alice);
+            }
+        }else{
+            best = Long.MAX_VALUE;
+            for(int x=1;x<=3 && x+i<=stoneValue.length;x++){
+                long alice = findAlice(i+x,0,stoneValue,dp);
+                best = Math.min(best,alice);
             }
         }
-
-        if (dp[0] > 0)
-            return "Alice";
-        else if (dp[0] < 0)
-            return "Bob";
-        else
-            return "Tie";
+        return dp[i][person] =best;
     }
 }
